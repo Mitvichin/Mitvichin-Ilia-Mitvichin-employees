acbf395c059b79db31172aa61ae939bc0e5103cf
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-08-14 22:19:22 +0300

    setup for eslint and prettier

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,11 @@
-import express, { Application, Response, Request } from "express";
+import express, { Application, Response, Request } from 'express';
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript + Express!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Express!');
 });
 
 export default app;
