5fe1dd04d437b3e9df5cb6e80d3b1094dcee8482
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-08-16 19:09:48 +0300

    added support for most common date formats

---
+++ backend/src/utils/parseDate.ts
@@ -0,0 +1,22 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { BackendError } from '../types/BackendError.js';
+
+dayjs.extend(customParseFormat);
+
+const dateFormats = ['YYYY-MM-DD', 'DD/MM/YYYY', 'MM/DD/YYYY', 'DD-MM-YYYY'];
+
+export const parseDate = (dateStr: string) => {
+  if (dateStr.toUpperCase() === 'NULL') {
+    return dayjs(new Date());
+  }
+  console.log(dateStr);
+  for (const format of dateFormats) {
+    const parsed = dayjs(dateStr, format, true);
+    if (parsed.isValid()) {
+      return parsed;
+    }
+  }
+
+  throw new BackendError(400, 'INVALID_DATE_FORMAT');
+};
