5673927137d1f0dc030f449b945c9877d1facba3
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-08-16 16:17:04 +0300

    added backend errors

---
+++ backend/src/constants/errorMessages.ts
@@ -0,0 +1,6 @@
+export const errorMessages = {
+  invalidCsv: 'INVALID_CSV',
+  parseError: 'PARSE_ERROR',
+  invalidFileExtension: 'INVALID_FILE_EXTENSION',
+  invalidFilseSize: 'INVALID_FILE_SIZE',
+} as const;
